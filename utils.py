408ea316bf095bcea8edc0cf8632e3515aa26cfd
Michael Engen <engenmt@gmail.com>
2023-06-16 17:46:22 -0400

    Added column, widened table cells, added fieldset formatting

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import asyncio
 import permpy as pp
+from math import pow
 
-from pyscript import Element, display
+# from pyscript import Element, display
 
 from js import document
 
@@ -26,42 +27,55 @@ def append_count_js(n, count):
     if n == 0:
         return
 
-    n = str(n)
-    count = str(count)
-
-    table = document.getElementById("table-enumeration")
-    attempt_to_log(table)
+    table = document.querySelector("#table-enumeration tbody")
 
     row = document.createElement("tr")
     row_id = f"row-{n}"
     row.id = row_id
-    attempt_to_log(row)
 
     cell_n = document.createElement("td")
     cell_n.id = f"n-{n}"
-    cell_n.innerHTML = n
-    attempt_to_log(cell_n)
+    cell_n.innerHTML = str(n)
 
     cell_count = document.createElement("td")
     cell_count.id = f"count-{n}"
-    cell_count.innerHTML = count
-    attempt_to_log(cell_count)
+    cell_count.innerHTML = str(count)
+
+    cell_count_root = document.createElement("td")
+    cell_count_root.id = f"count-root-{n}"
+    cell_count_root.innerHTML = f"{pow(count, 1 / n): 6.3f}"
 
     row.append(cell_n)
     row.append(cell_count)
-    attempt_to_log(row)
+    row.append(cell_count_root)
+
     table.append(row)
-    attempt_to_log(table)
+
+
+def reset_table():
+    while elt := document.querySelector("tr:not(:first-child)"):
+        elt.remove()
 
 
 async def enumerate_by_basis(basis, N):
     N = int(N)
+    if N >= 10:
+        return
+    document.querySelector("button").disabled = True
+    reset_table()
+
     basis = [pp.Perm(elt.strip()) for elt in basis.split(",")]
     A = pp.AvClass(basis, max_len=0)
     for n in range(1, N + 1):
         await asyncio.sleep(0.01)
         A.extend_by_one()
         append_count_js(n, len(A[-1]))
+
+    document.querySelector("button").disabled = False
+    # await asyncio.sleep(0.01)
+
+
+document.querySelector("button").disabled = False
 
 
 def main():
